1205b7fccbfdbfa23c4f7d5ebbbaea2684d1acbd
Mate Antunovic <mate.a@blackpearlmail.com>
2015-07-21 19:15:22 +1200

    Make program have more skeleton functionality

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,8 @@
-import Github.Repos
+import qualified Github.Repos as Repos
+import qualified Github.PullRequests as Pulls
+import qualified Github.Data.Definitions as GitData
 import System.Environment
+import Control.Applicative
 
 main :: IO ()
 main = do
@@ -8,12 +11,24 @@ main = do
 	program (progName:args)
 
 type Username = [Char]
-type Password = [Char]
-type RepoName = [Char]
+type Password = Username
+type RepoName = Password
 
 program :: [String] -> IO ()
 program [_, username, password, repository] = runBot (username, password) repository
-program (progName:_) = putStrLn $ "Usage: " ++ progName ++ " USERNAME PASSWORD REPONAME" 
+program (progName:_) = putStrLn $ "Usage: " ++ progName ++ " USERNMAE PASSWORD REPONAME" 
 
 runBot :: (Username, Password) -> RepoName -> IO ()
-runBot _ _ = putStrLn "Duhhhh... I need to do stuff"
+runBot (username, _) repoName = processRepo username repoName
+
+processRepo :: Username -> RepoName -> IO ()
+processRepo username repoName = do 
+		pullRequests <- (Pulls.pullRequestsFor username repoName) 
+		if (hasAppropriateGif pullRequests) then putStrLn "This pull request has a GIF." else putStrLn "This pull request needs a GIF folks!"
+
+hasAppropriateGif ::  Either GitData.Error [GitData.PullRequest]  -> Bool
+hasAppropriateGif (Right pullRequest) = any (anyCommentsHaveGif) pullRequest
+hasAppropriateGif (Left _) = True
+
+anyCommentsHaveGif :: GitData.PullRequest -> Bool
+anyCommentsHaveGif pullRequest = True
